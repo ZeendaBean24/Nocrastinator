17afa0ce5dc77ac792bd248c9e6cf69f90ad4286
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-09 12:38:14 -0500

    Add build for frontend // Quote cannot parse JSON

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3001;
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
--- frontend/src/Components/Quotes/index.js
+++ frontend/src/Components/Quotes/index.js
@@ -3,23 +3,25 @@ import { QuoteContainer, QuoteText, AuthorText, CategoryText } from './styles';
 
 const QuoteDisplay = () => {
     const [quoteData, setQuoteData] = useState({ quote: '', author: '', category: '' });
+    const [error, setError] = useState('');
 
-    useEffect(() => {
-        const fetchQuote = async () => {
-            try {
-                const response = await fetch('/api/quote');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                setQuoteData(data);
-            } catch (error) {
-                console.error('Error fetching the quote:', error);
+
+    fetch('/api/quote')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
             }
-        };
+            return response.json();
+        })
+        .then(data => setQuoteData(data))
+        .catch(error => {
+            console.error('Error fetching the quote:', error);
+            setError(error.message);
+        });
 
-        fetchQuote();
-    }, []);
+    if (error) {
+        return <div>Error loading quote: {error}</div>;
+    }
 
     return (
         <QuoteContainer>
