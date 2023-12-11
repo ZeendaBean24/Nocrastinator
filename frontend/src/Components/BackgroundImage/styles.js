a3985a507ad6384a03de9ac25e70c6fcd9c10163
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-10 22:39:03 -0500

    Make noise filter a lot darker using colour matrix

--- frontend/src/Components/BackgroundImage/styles.js
+++ frontend/src/Components/BackgroundImage/styles.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 function SVGNoiseFilter() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: 'none' }}>
+        <svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}>
             <defs>
                 <filter id="grainyNoise">
-                    <feTurbulence type="fractalNoise" baseFrequency="0.8" numOctaves="10" result="noise" />
-                    <feComposite in="SourceGraphic" in2="noise" operator="in" />
+                    <feTurbulence type="fractalNoise" baseFrequency="0.8" numOctaves="3" result="noise"/>
+                    <feColorMatrix in="noise" type="matrix"
+                        values="1 0 0 0 0
+                                0 1 0 0 0
+                                0 0 1 0 0
+                                0 0 0 30 -15" result="darkerNoise"/>
+                    <feBlend in="SourceGraphic" in2="darkerNoise" mode="multiply"/>
                 </filter>
             </defs>
         </svg>
