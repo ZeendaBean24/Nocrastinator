2e2176454411a68758f72bfd2d4c1a1fe893f115
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-13 21:17:50 -0500

    Add neumorphism effect to clock light mode

--- frontend/src/Components/Clock/styles.js
+++ frontend/src/Components/Clock/styles.js
@@ -5,13 +5,10 @@ export const LightClockContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 250px;
-  height: 250px;
-  /* width: auto;
-  height: 20vh; */
+  width: auto;
+  height: 10vh;
   border-radius: 50px;
-  background-color: #E5E0FF;
-  cursor: pointer;
+  background-color: powderblue;
   position: relative;
   box-shadow: 30px 30px 30px -10px rgba(0, 0, 0, 0.15),
       inset 15px 15px 10px rgba(255, 255, 255, 0.75),
@@ -20,22 +17,44 @@ export const LightClockContainer = styled.div`
 `;
 
 export const LightClockText = styled.h1`
-  border: 2px solid burlywood;
-  width: 250px;
-  margin: 2vh auto 0px auto;
-  border-radius: 15px;
-  padding: 10px 0;
-  background-color: cornsilk;
-  box-shadow: rgba(255, 255, 255, 0.35) 0px 5px 15px;
-  color: sandybrown;
+  font-size: 3rem; /* Adjust the font size as needed */
+  color: midnightblue; /* Text color is midnight blue */
+  text-align: center; /* Center-align the text horizontally */
+  padding: 20px; /* Add padding to the text for better appearance */
+  border-radius: 50px;
+  position: relative; /* Enable positioning */
+  text-shadow: 2px 3px 6px 
+            rgba(0, 0, 0, 0.5),  
+            -2px -2px 0 
+            rgba(255, 255, 255, 0.8); 
 `;
 
-// Dark theme styles
+// Dark theme styles with a larger gleam at the top left corner
 export const DarkClockContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: auto;
   height: 20vh;
-  text-align: center;
-  background-color: midnightblue; /* Set dark theme background color */
+  border-radius: 50px;
+  background-color: midnightblue;
+  cursor: pointer;
+  position: relative;
+  box-shadow:
+    10px 10px 20px rgba(0, 0, 0, 0.2), /* Outer shadow */
+    -10px -10px 20px rgba(255, 255, 255, 0.1); /* Inner shadow */
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: -20px; /* Adjust the top position to make the gleam larger */
+    left: -20px; /* Adjust the left position to make the gleam larger */
+    right: 5px;
+    bottom: 5px;
+    background: linear-gradient(45deg, rgba(0, 0, 0, 0.05), rgba(255, 255, 255, 0.05));
+    border-radius: 50px;
+    z-index: -1;
+  }
 `;
 
 export const DarkClockText = styled.h1`
