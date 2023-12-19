81adffa8a8e51ec93464ce652e5fe76a40129de5
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-18 21:04:04 -0500

    Add depth and neumorphism to toggle button, add animation

--- frontend/src/Components/BackgroundColor/styles.js
+++ frontend/src/Components/BackgroundColor/styles.js
@@ -11,50 +11,50 @@ export const LightColorOptionsButton = styled.button`
   cursor: pointer;
   height: 100px;
   width: 100px;
-  box-shadow:
-  -10px -10px 15px rgba(255,255,255,0.5),
-  10px 10px 15px rgba(70,70,70,0.12);
-  position: absolute;
+  background-color: #f0f0f0;
   border-radius: 50%;
-  border: 8px solid #ececec;
+  border: none;
   outline: none;
-`
+  position: relative;
+  transition: all 0.3s ease-in-out;
+  box-shadow:
+    -10px -10px 20px rgba(255, 255, 255, 0.7),
+    10px 10px 20px rgba(0, 0, 0, 0.2),
+    inset -5px -5px 10px rgba(255, 255, 255, 0.7),
+    inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
 
 export const DarkColorOptionsButton = styled.button`
   cursor: pointer;
   height: 100px;
   width: 100px;
-  box-shadow:
-  -10px -10px 15px rgba(255,255,255,0.5),
-  10px 10px 15px rgba(70,70,70,0.12),
-  inset -10px -10px 10px rgba(255,255,255,0.5),
-  inset 10px 10px 10px rgba(70,70,70,0.12);
-  position: absolute;
+  background-color: #f0f0f0;
+  color: #fff; /* Set text color to white */
   border-radius: 50%;
-  border: 8px solid #ececec;
+  border: none;
   outline: none;
-`
+  position: relative;
+  transition: all 0.3s ease-in-out;
+  box-shadow:
+    -10px -10px 20px rgba(80, 80, 80, 0.7),
+    10px 10px 20px rgba(0, 0, 0, 0.2),
+    inset -5px -5px 10px rgba(80, 80, 80, 0.7),
+    inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
 
 export const LightPowerSVG = styled.svg`
   fill: none;
-`
+`;
 
 export const DarkPowerSVG = styled.svg`
   fill: none;
-`
-
-
-
-// export const ColorOption = styled.div`
-//   width: 40px;
-//   height: 40px;
-//   margin: 10px 8px;
-//   cursor: pointer;
-// `;
-
-// export const ColorHeading = styled.h2`
-//     margin: 0;
-//     text-align: center;
-// `
-
-// Additional styling can be added here as needed
+  filter: drop-shadow(0 0 5px rgba(21,227,138, 0.8)); 
+`;
