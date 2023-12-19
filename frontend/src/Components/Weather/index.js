d15264eb437e070f372810dcf8c36ae3da1aecae
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-18 22:00:27 -0500

    Resize weather component and make all components responsive (em)

--- frontend/src/Components/Weather/index.js
+++ frontend/src/Components/Weather/index.js
@@ -133,7 +133,7 @@ function WeatherApp() {
         <TopBar>
             <CityInput type="text" value={inputVal} onChange={handleSearch}/>
             <SearchIcon onClick={() => {Search()}}>
-                <img src={search_icon} alt="" />
+                <img src={search_icon} alt=""/>
             </SearchIcon>
         </TopBar>
         <WeatherImage>
--- frontend/src/Components/Weather/styles.js
+++ frontend/src/Components/Weather/styles.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 export const WeatherContainer = styled.div`
-    width: 225px;
-    height: 400px;
-    /* width: 300px;
-    height: 600px; */
+    width: 150px;
+    height: 235px;
     margin: auto;
-    margin-top: 20px;
+    font-size: 12px;
+    margin-top: 1.25em; /* Adjusted margin in em */
     border-radius: 12px;
     background-image: linear-gradient(180deg, #130754 0%, #3b2f80 100%);
 `
@@ -14,21 +13,21 @@ export const WeatherContainer = styled.div`
 export const TopBar = styled.div`
     display: flex;
     justify-content: center;
-    gap: 10px;
-    padding-top: 30px;
+    gap: 0.825em; /* Adjusted gap in em */
+    padding-top: 1.275em; /* Adjusted padding in em */
 `
 
 export const CityInput = styled.input`
     display: flex;
-    width: 110px;
-    height: 39px;
+    width: 7.1875em; /* Adjusted width in em */
+    height: 2.34375em; /* Adjusted height in em */
     background: #ebfffc;
     border: none;
     outline: none;
-    border-radius: 40px;
-    padding-left: 20px;
+    border-radius: 2.5em; /* Adjusted border-radius in em */
+    padding-left: 1.25em; /* Adjusted padding in em */
     color: #626262;
-    font-size: 12px;
+    font-size: 0.875em; /* Relative font size in em */
     font-weight: 400;
 `
 
@@ -36,16 +35,17 @@ export const SearchIcon = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 39px;
-    height: 39px;
+    width: 2.34375em; /* Adjusted width in em */
+    height: 2.34375em; /* Adjusted height in em */
     background: #ebfffc;
-    border-radius: 40px;
+    border-radius: 2.5em; /* Adjusted border-radius in em */
     cursor: pointer;
 `
+
 export const WeatherImage = styled.div`
-    margin-top: 15px;
+    margin-top: 0.25em; /* Adjusted margin in em */
     display: flex;
-    height: 100px;
+    height: 4.25em; /* Adjusted height in em */
     justify-content: center;
     align-content: center;
 `
@@ -54,8 +54,8 @@ export const WeatherTemp = styled.div`
     display: flex;
     justify-content: center;
     color: white;
-    height: 35px;
-    font-size: 30px;
+    height: 1.075em; /* Adjusted height in em */
+    font-size: 2em; /* Relative font size in em */
     font-weight: 400;
 `
 
@@ -63,18 +63,19 @@ export const WeatherLocation = styled.div`
     display: flex;
     justify-content: center;
     color: white;
-    height: 25px;
-    font-size: 25px;
+    height: 1.25em; /* Adjusted height in em */
+    font-size: 0.9375em; /* Relative font size in em */
+    margin-top: 0.1em;
     font-weight: 400;
 `
 
 export const DataContainer = styled.div`
-    margin-top: 25px;
+    margin-top: 0.9625em; /* Adjusted margin in em */
     color: white;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    gap: 15px;
+    gap: 0.6375em; /* Adjusted gap in em */
 `
 
 export const HumidityElement = styled.div`
@@ -83,7 +84,7 @@ export const HumidityElement = styled.div`
     justify-content: center;
     align-items: center;
     width: 100%;
-    gap: 15px;
+    gap: 0.9375em; /* Adjusted gap in em */
 `
 
 export const WindElement = styled.div`
@@ -92,11 +93,11 @@ export const WindElement = styled.div`
     justify-content: center;
     align-items: center;
     width: 100%;
-    gap: 15px;
+    gap: 0.9375em; /* Adjusted gap in em */
 `
 
 export const WeatherData = styled.div`
-    font-size: 15px;
+    font-size: 0.875em; /* Relative font size in em */
     font-weight: 400;
 `
 
@@ -109,11 +110,11 @@ export const WindRate = styled.div`
 `
 
 export const WeatherText = styled.div`
-    font-size: 15px;
+    font-size: 0.875em; /* Relative font size in em */
     font-weight: 400;
 `
 
 export const WeatherIcon = styled.img`
-    margin-top: 10px;
-    width: 25px;
+    margin-top: 0.625em; /* Adjusted margin in em */
+    width: 1.5625em; /* Adjusted width in em */
 `
