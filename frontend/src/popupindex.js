8502b814f07dd7de627ddb20d8ff771f18a59dcd
ZeendaBean24 <liuzeen999@gmail.com>
2024-01-16 19:01:11 -0500

    Test 1, trying to make it work, start checkpoint

--- frontend/src/popupindex.js
+++ frontend/src/popupindex.js
@@ -3,9 +3,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Popup from './popup'; // Adjust the path as needed
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Popup />
-  </React.StrictMode>,
-  document.getElementById('popup-root')
-);
+ReactDOM.render(<Popup />, document.getElementById('popup'));
+
