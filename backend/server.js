c3bd2e3603f726671d0c82b635aa590036a5d2c8
ZeendaBean24 <liuzeen999@gmail.com>
2024-02-28 17:57:37 -0500

    Update backend (minor changes)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch');
 const app = express();
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3001;
+
 const cors = require('cors');
 app.use(cors());
 
@@ -61,6 +63,6 @@ app.get('/wordOfTheDay', async (req, res) => {
     }
 });
 
-app.listen(process.env.DEPLOYMENT_URL, () => {
-    console.log(`Server running on port ${process.env.DEPLOYMENT_URL}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
